94e424183e4129f40d2af2093db65c17be59107d
TugasAkhir01 <sta046513@gmail.com>
2025-07-26 16:14:42 +0700

    Update LoginForm.js

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -51,6 +51,7 @@ const LoginForm = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ identifier, password }),
+        credentials: 'include',
       });
 
       const data = await response.json();
